a6f2d3a4856696922f88dbdf02c4e1217ac63f2f
Alistair Chapman <achapman@redhat.com>
2017-04-18 17:04:07 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello {System.Environment.GetEnvironmentVariable("USER")}! I'm {System.Environment.MachineName} and I'm talking to you from {System.IO.Directory.GetCurrentDirectory()}");
+        }
+    }
+}
